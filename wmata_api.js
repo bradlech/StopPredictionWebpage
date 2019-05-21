901852683e17321223b996a208840446dfe6eba6
Cole Bradley <cole.hamilton.bradley@gmail.com>
2019-05-20 20:54:49 -0400

    Put the JS in its own file

---
+++ wmata_api.js
@@ -0,0 +1,24 @@
+function test() {	
+	var today = new Date();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var params = {
+		"api_key": "e13626d03d8e4c03ac07f95541b3091b",
+		// Request parameters
+		"StopID": "6000615",
+	};
+  
+	$.ajax({
+		url: "https://api.wmata.com/NextBusService.svc/json/jPredictions?" + $.param(params),
+		type: "GET",
+	})
+	.done(function(data) {
+		console.log(data);
+		document.getElementById("last_refresh").innerHTML = time;
+		document.getElementById("stop_1").innerHTML = data.Predictions[0].RouteID + " - " + data.Predictions[0].DirectionText;
+		document.getElementById("stop_1_time").innerHTML = data.Predictions[0].Minutes;
+		//alert("success");
+	})
+	.fail(function() {
+		alert("error");
+	});
+}
