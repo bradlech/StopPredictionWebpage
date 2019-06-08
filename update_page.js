497cb59136fd6272cdf3a970c2ed691224ab2cee
Cole Bradley <cole.hamilton.bradley@gmail.com>
2019-06-08 09:40:43 -0400

    Added transit logos and future bus stops

--- update_page.js
+++ update_page.js
@@ -1,13 +1,13 @@
 function displayBusTimes(nextBuses) {
     const busPredictions = nextBuses.Predictions;
-    const routeDataId = "#stop-route-data";
+    const routeDataId01 = "#stop-route-data-1";
     const minutesId = "#stop-time";
 
-    $(routeDataId).empty();
+    $(routeDataId01).empty();
     $(minutesId).empty();
     $("#last_refresh").text(new Date().toLocaleTimeString());
     if (busPredictions.length == 0) {
-        $(routeDataId).text("No route data available");
+        $(routeDataId01).text("No route data available");
     }
     else {
         busPredictions.forEach((bus) => {
@@ -18,7 +18,7 @@ function displayBusTimes(nextBuses) {
             const route = bus.RouteID;
             const mins = (min == 1) ? "minute" : "minutes";
 
-            $(routeDataId).append(
+            $(routeDataId01).append(
                 "<li class='list-group-item'><span class='font-weight-bold'>Bus " + id +
                 "</span> will arrive in approximately <span class='font-weight-bold'>" + min +
                 " " + mins + "</span></li>"
